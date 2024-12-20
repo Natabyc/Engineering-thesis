8c6ce7f678ccb32b1b1cc14ee73e681cb782fce2
Natabyc <72974869+Natabyc@users.noreply.github.com>
2024-12-20 18:00:10 +0100

    add dos protection initial testint

---
+++ DoS_Protection_initial_testing.py
@@ -0,0 +1,118 @@
+import xml.etree.ElementTree as ET
+from panos import firewall
+
+# connect with device - type in correct credentials
+fw = firewall.Firewall("10.74.1.17", "admin", "AdminXYZ123")
+
+#defining DoS Protection and DoS Profile parameters
+profile_name = "DoS_protect" #add suffix with attack name to know which protection is enabled
+policy_name = "DoS_protect"
+enable_synflood = 'yes' #Syn flood protection was used in initial testing
+enable_udpflood = 'no'
+enable_icmpflood = 'no'
+alarm_rate = '100'
+activate_rate = '1'
+maximal_rate = '1000'
+zone_from = 'WAN'
+zone_to = 'DMZ'
+
+#creating xml structure to add DoS Protection Profile
+#values of parameters need to be changed in xml structure, cannot be saved to variables (error "invalid")
+xml_payload = f'''
+<entry name="{profile_name}">
+    <flood>
+        <tcp-syn>
+            <red>
+                <alarm-rate>{alarm_rate}</alarm-rate>
+                <activate-rate>{activate_rate}</activate-rate>
+                <maximal-rate>{maximal_rate}</maximal-rate>
+            </red>
+        <enable>{enable_synflood}</enable>
+        </tcp-syn>
+        <udp>
+            <red>
+                <alarm-rate>{alarm_rate}</alarm-rate>
+                <activate-rate>{activate_rate}</activate-rate>
+                <maximal-rate>{maximal_rate}</maximal-rate>
+            </red>
+        <enable>{enable_udpflood}</enable>
+        </udp>
+        <icmp>
+            <red>
+                <alarm-rate>{alarm_rate}</alarm-rate>
+                <activate-rate>{activate_rate}</activate-rate>
+                <maximal-rate>{maximal_rate}</maximal-rate>
+            </red>
+        <enable>{enable_icmpflood}</enable>
+        </icmp>
+        <icmpv6>
+            <red>
+                <alarm-rate>{alarm_rate}</alarm-rate>
+                <activate-rate>{activate_rate}</activate-rate>
+                <maximal-rate>{maximal_rate}</maximal-rate>
+            </red>
+        <enable>{enable_icmpflood}</enable>
+        </icmpv6>      
+    </flood>
+        <resource>
+        <sessions>
+        <enabled>yes</enabled>
+        </sessions>
+        </resource>
+        <type>aggregate</type>  
+</entry>
+'''
+
+#specify DoS Protection profile location where the API request will apply the changes
+xpath = f"/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name='vsys1']/profiles/dos-protection"
+response = fw.xapi.set(xpath=xpath, element=xml_payload)
+print(response)
+
+response_str = ET.tostring(response, encoding="unicode")
+print("Detailed response DoS_profile XML:", response_str)
+
+
+#creating xml structure to add DoS Protection rule
+xml_payload_policy = f""""
+<entry name="{policy_name}">
+    <from>
+    <zone>
+        <member>{zone_from}</member>
+    </zone>
+    </from>
+    <to>
+    <zone>
+        <member>{zone_to}</member>
+    </zone>
+    </to>
+    <protection>
+        <aggregate>
+    <profile>{profile_name}</profile>
+    </aggregate>
+    </protection>
+    <source>
+        <member>any</member>
+    </source>
+    <destination>
+        <member>any</member>
+    </destination>
+    <source-user>
+        <member>any</member>
+    </source-user>
+    <service>
+        <member>any</member>
+    </service>
+    <action>
+        <protect/>
+    </action>
+</entry>
+ """
+
+#specify DoS Protection rule location where the API request will apply the changes
+xpath_profile = f"/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name='vsys1']/rulebase/dos/rules"
+
+response_profile = fw.xapi.set(xpath=xpath_profile, element=xml_payload_policy)
+print(response_profile)
+
+response_str_profile = ET.tostring(response_profile, encoding="unicode")
+print("Detailed response DoS_policy XML:", response_str_profile)
